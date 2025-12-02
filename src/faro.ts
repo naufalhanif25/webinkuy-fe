9bb2b4f89b462c1cfc4178aa0210af19de540a87
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-12-02 21:31:26 +0700

    feat: add UserActionIntrumentation to initializeFaro

--- src/faro.ts
+++ src/faro.ts
@@ -1,19 +1,17 @@
-import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
-  import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+import { getWebInstrumentations, initializeFaro, UserActionInstrumentation } from "@grafana/faro-web-sdk";
+import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 
-  initializeFaro({
-    url: 'https://faro-collector-prod-ap-southeast-2.grafana.net/collect/3f07a4ab98e88085dc025d26eb150d8d',
+export const faro = initializeFaro({
+    url: "https://faro-collector-prod-ap-southeast-2.grafana.net/collect/3f07a4ab98e88085dc025d26eb150d8d",
     app: {
-      name: 'WebinKuy',
-      version: '1.0.0',
-      environment: 'production'
+        name: "WebinKuy",
+        version: "1.0.0",
+        environment: "production",
     },
     
     instrumentations: [
-      // Mandatory, omits default instrumentations otherwise.
-      ...getWebInstrumentations(),
-
-      // Tracing package to get end-to-end visibility for HTTP requests.
-      new TracingInstrumentation(),
+        ...getWebInstrumentations(),
+        new TracingInstrumentation(),
+        new UserActionInstrumentation(),
     ],
-  });
+});
